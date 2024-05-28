0ab1286f2801df8873b6af492bf5dd7d1ac7138c
VitoMarino <marino.vito2001@gmail.com>
2024-05-28 12:59:14 +0200

    vue

---
+++ script.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+    return {
+        message: 'Hello Vue!',
+        img: 'iron-man'
+    }
+},
+methods: {
+    miaFunzione: function() {
+        
+    }
+}
+}).mount('#app')
